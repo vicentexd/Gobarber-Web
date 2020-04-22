8439f955ec83b351fd4b5daf208d62234357a61e
Vicente Dev <vicente.gabrielv@gmail.com>
2020-04-22 20:34:30 -0300

    Começo Athenticate

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import GlobalStyled from './styles/global';
 import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
     <GlobalStyled />
+
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
   </>
 );
 
